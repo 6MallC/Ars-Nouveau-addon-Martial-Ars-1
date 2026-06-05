540ce7f2b7e65726da3ab51fe4caf9b91fa3bd97
Connor M <conbrum2006@gmail.com>
2026-06-04 23:28:24 -0400

    datagen help me

--- src/main/java/com/connorm/martial_ars/ExampleConfig.java
+++ src/main/java/com/connorm/martial_ars/ExampleConfig.java
@@ -5,7 +5,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+
 public class ExampleConfig {
 
     @SubscribeEvent
--- src/main/java/com/connorm/martial_ars/datagen/ModRecipeProvider.java
+++ src/main/java/com/connorm/martial_ars/datagen/ModRecipeProvider.java
@@ -1,4 +1,41 @@
 package com.connorm.martial_ars.datagen;
 
-public class ModRecipeProvider {
+import com.connorm.martial_ars.ArsNouveauRegistry;
+import com.connorm.martial_ars.registry.ModRegistry;
+import com.hollingsworth.arsnouveau.common.datagen.RecipeDatagen;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+
+    public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries);
+    }
+
+    @Override
+    protected void buildRecipes(RecipeOutput recipeOutput) {
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModRegistry.MushroomWand.get())
+                .pattern("CBC")
+                .pattern(" A ")
+                .pattern(" A ")
+                .define('A',ModRegistry.SourceIngot.get())
+                .define('B', RecipeDatagen.SOURCE_GEM)
+                .define('C', Tags.Items.MUSHROOMS)
+                .unlockedBy("has_sourceingot", has(ModRegistry.SourceIngot)).save(recipeOutput);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModRegistry.SourceIngot.get(),9)
+                .requires(ModRegistry.sourceingot_block)
+                .unlockedBy("has_sourceingot_block", has(ModRegistry.sourceingot_block)).save(recipeOutput);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModRegistry.sourceingot_block.get())
+                .requires(ModRegistry.SourceIngot,9)
+                .unlockedBy("has_source_ingot", has(ModRegistry.SourceIngot)).save(recipeOutput);
+
+    }
 }
