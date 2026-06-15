112870060f2ef4a5dd70003b537a2a44f4408798
Connor M <conbrum2006@gmail.com>
2026-06-15 18:04:04 -0400

    Kunai now has stats spear now can regen issues are with the caster component

--- src/main/java/com/connorm/martial_ars/entity/ModEntities.java
+++ src/main/java/com/connorm/martial_ars/entity/ModEntities.java
@@ -1,4 +1,18 @@
 package com.connorm.martial_ars.entity;
 
+import com.connorm.martial_ars.MartialArs;
+import com.connorm.martial_ars.entity.custom.KunaiProjectileEntity;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
 public class ModEntities {
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
+            DeferredRegister.create(BuiltInRegistries.ENTITY_TYPE, MartialArs.MODID);
+    public static final Supplier<EntityType<KunaiProjectileEntity>> KUNAI =
+            ENTITY_TYPES.register("kunai",() -> EntityType.Builder.of(KunaiProjectileEntity::new, MobCategory.MISC)
+                    .sized(0.5f,1.15).build("kunai"));
 }
