8d4660cd55caa75c08d43b5695c70dbb5b50107e
Connor M <conbrum2006@gmail.com>
2026-09-01 01:43:15 -0400

    Ritual is real real still have some kinks to work out tho + model/name

--- src/main/java/com/connorm/martial_ars/ritual/RitualRepair.java
+++ src/main/java/com/connorm/martial_ars/ritual/RitualRepair.java
@@ -83,7 +83,7 @@ public class RitualRepair extends AbstractRitual implements IPedestalMachine {
     @Override
     public String getLangDescription(){
         return "Uses a moderate amount of source per second to repair anything with durability" +
-                " placed on adjacent arcane pedestals, Unless you augment it with earth and air essence in which case;" +
+                " placed on adjacent arcane pedestals, Unless you augment it with earth and air essence in which case" +
                 " it will instead apply an effect in a wide area that repairs tools in equipment slots" +
                 "  (armor slots off hand and main hand) though at halved efficiency." +
                 " fire essence can be used to increase throughput consuming more source for more durability per second.";
